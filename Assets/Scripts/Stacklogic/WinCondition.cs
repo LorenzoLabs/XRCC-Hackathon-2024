666af4ba39c1c87961dfb541d67499a67352b230
yolanda <yolanda@runnningwild.io>
2024-06-29 14:13:51 +0100

    Added Mika's spawn logic

---
+++ Assets/Scripts/Stacklogic/WinCondition.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    public bool isWon;
+
+    private bool isTimerRunning;
+    private float timer;
+    private float timerThreshold = 3f; 
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("grabbable"))
+        {
+            isTimerRunning = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("grabbable"))
+        {
+            isTimerRunning = false;
+            timer = 0f; // Reset timer when the grabbable object exits
+        }
+    }
+
+    private void Start()
+    {
+        isWon = false;
+        isTimerRunning = false;
+        timer = 0f;
+    }
+
+    void Update()
+    {
+        if (isTimerRunning)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= timerThreshold)
+            {
+                isWon = true;
+                // Optionally, you might want to call a method like setbool(true) here
+            }
+        }
+        else
+        {
+            timer = 0f; // Reset timer if no grabbable object is inside
+            isWon = false;
+        }
+    }
+}
