d1824c7cda7b6e4d55de7646b4679037f181d488
yolanda <yolanda@runnningwild.io>
2024-06-29 15:54:57 +0100

    Removed unused import that caused build error

--- Assets/Scripts/Stacklogic/SpawnStackLocation.cs
+++ Assets/Scripts/Stacklogic/SpawnStackLocation.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Meta.XR.MRUtilityKit;
-using UnityEditor.Animations;
 
 public class SpawnStackLocation : MonoBehaviour
 {
